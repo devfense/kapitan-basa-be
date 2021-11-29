9a0f4645aa502a5e889a83e23746941cb2f00f5e
jeph-kun <jephv4@gmail.com>
2021-11-29 17:42:37 +0800

    user mgmt dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,15 @@ import { StudentModule } from './student/student.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({ 
        type: 'postgres',
-       host: process.env.POSTGRES_HOST,
-       port: parseInt(<string>process.env.POSTGRES_PORT),
-       username: process.env.POSTGRES_USER,
-       password: process.env.POSTGRES_PASSWORD,
-       database: process.env.POSTGRES_DATABASE,
-       autoLoadEntities: true,
-       //REMOVE THIS IN PRODUCTION
-       synchronize: true,
+       url: process.env.DATABASE_URL
+      //  host: process.env.POSTGRES_HOST,
+      //  port: parseInt(<string>process.env.POSTGRES_PORT),
+      //  username: process.env.POSTGRES_USER,
+      //  password: process.env.POSTGRES_PASSWORD,
+      //  database: process.env.POSTGRES_DATABASE,
+      //  autoLoadEntities: true,
+      //  //REMOVE THIS IN PRODUCTION
+      //  synchronize: true,
 
     }),
     StudentModule
